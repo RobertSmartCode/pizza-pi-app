084e6992e1681ccbb6c3eadc56d18b46df4a06ac
Robert Smart <roca1388@gmail.com>
2022-01-10 13:14:59 -0300

    Actualizar

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -95,7 +95,7 @@ const Product = ({ pizza }) => {
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `http://localhost:3000/api/products/${params.id}`
+    `https://pizza-pi-app.herokuapp.com/api/products/${params.id}`
   );
   return {
     props: {
